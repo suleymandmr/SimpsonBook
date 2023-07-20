5a36feecf1cad7c05e1e2e1ebad4a5e448d3c79b
Süleyman Demir <suleymandemir75@hotmail.com>
2023-07-20 21:49:01 +0300

    Initial Commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,26 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Atil Samancioglu on 11.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+    }
+    
+}
---
+++ SimpsonBook/detailVC.swift
@@ -0,0 +1,31 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Atil Samancioglu on 11.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
